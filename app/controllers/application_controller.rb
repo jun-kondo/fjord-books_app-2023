23416f77908b66d30e43cc47056a296704e52e4d
jun-kondo <ranmaru.oo.q@gmail.com>
2024-03-01 19:17:25 +0900

    [Modify]login, logout後の画面遷移を変更
    login→本一覧、logout→ログイン画面に移動するよう変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(_resource)
+    books_url
+  end
+
+  def after_sign_out_path_for(_resource)
+    # flash[:notice] = t "devise.sessions.signed_out"
+    new_user_session_url
+  end
 end
