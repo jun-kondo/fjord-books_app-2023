d8196bd16a08acf7c11aa59e3fc52e7df64548d1
jun-kondo <ranmaru.oo.q@gmail.com>
2024-03-14 03:39:34 +0900

    [Add]画像アップロードのバリデーションを追加
    active_storage_validations gemを導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@ class User < ApplicationRecord
   has_one_attached :image do |attachable|
     attachable.variant :display, resize_to_limit: [100, 100]
   end
+  validates :image, content_type: %w[image/jpeg image/png image/gif],
+                    size: { less_than: 5.megabytes }
 end
