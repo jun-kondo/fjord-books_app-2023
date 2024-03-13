a94a7a29c33e2a2e8d475c6007a711e0b17113b4
jun-kondo <ranmaru.oo.q@gmail.com>
2024-03-14 02:06:20 +0900

    [Add]image_processing導入で画像をリサイズ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,7 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :display, resize_to_limit: [100, 100]
+  end
 end
