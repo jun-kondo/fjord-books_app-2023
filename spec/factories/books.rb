00018434a4ec88e110e5809db00f25e8aadfcb6f
jun-kondo <ranmaru.oo.q@gmail.com>
2024-03-21 11:54:02 +0900

    [Delete].rspec specファイルを追跡対象外に設定

--- spec/factories/books.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :book do
-    title { Faker::Book.title }
-    memo { Faker::Book.genre }
-    author { Faker::Book.author }
-  end
-end
--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory :user do
-    email { Faker::Internet.email }
-    password { 'password' }
-    password_confirmation { 'password' }
-  end
-end
